19db35cc61e8762f93cbc94d39cc73c22bcd22e5
diennq <diennq@tohsoft.com>
2026-01-07 16:34:38 +0700

    Initial commit

---
+++ H2Only/H2OnlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  H2OnlyApp.swift
+//  H2Only
+//
+//  Created by Trangptt on 7/1/26.
+//
+
+import SwiftUI
+
+@main
+struct H2OnlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
